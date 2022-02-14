f33b26829caa66646a0e422735115a671861daf2
BeingMighty <98230407+BeingMighty@users.noreply.github.com>
2022-02-15 01:49:41 +0530

    Update updater.py

--- MightyXSpam/plugins/updater.py
+++ MightyXSpam/plugins/updater.py
@@ -13,7 +13,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/BeingMighty/MightyXSpam"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/BeingMighty/MightyXIDSpam"
 BOT_IS_UP_TO_DATE = "**The Mighty X Spam** is up-to-date sur."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
