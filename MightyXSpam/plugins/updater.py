91802a8d46c81eae962152f906fa10fceed11d9a
Mighty <98230407+BeingMighty@users.noreply.github.com>
2022-03-06 15:43:08 +0530

    Update updater.py

--- MightyXSpam/plugins/updater.py
+++ MightyXSpam/plugins/updater.py
@@ -1,33 +1,10 @@
 import asyncio
 import os
 import sys
-import git
 from telethon import events
-from MightyXSpam import Mig, Mig2, Mig3, Mig4, Mig5 , Mig6, Mig7, Mig8, Mig9, Mig10, Mig11, Mig12, Mig13, Mig14, Mig15, Mig16, Mig17, Mig18, Mig19, Mig20, Mig21, Mig22, Mig23, Mig24, Mig25, Mig26, Mig27, Mig28, Mig29, Mig30, Mig31, Mig32, Mig33, Mig34, Mig35, Mig36, Mig37, Mig38, Mig39, Mig40, DEV, HEROKU_APP_NAME, HEROKU_API_KEY
+from MightyXSpam import Mig, Mig2, Mig3, Mig4, Mig5 , Mig6, Mig7, Mig8, Mig9, Mig10, Mig11, Mig12, Mig13, Mig14, Mig15, Mig16, Mig17, Mig18, Mig19, Mig20, Mig21, Mig22, Mig23, Mig24, Mig25, Mig26, Mig27, Mig28, Mig29, Mig30, Mig31, Mig32, Mig33, Mig34, Mig35, Mig36, Mig37, Mig38, Mig39, Mig40, DEV
 from .. import CMD_HNDLR as hl
 
-# -- Constants -- #
-IS_SELECTED_DIFFERENT_BRANCH = (
-    "looks like a custom branch {branch_name} "
-    "is being used:\n"
-    "in this case, Updater is unable to identify the branch to be updated."
-    "please check out to an official branch, and re-start the updater."
-)
-OFFICIAL_UPSTREAM_REPO = "https://github.com/BeingMighty/MightyXIDSpam"
-BOT_IS_UP_TO_DATE = "**The Mighty X Spam** is up-to-date sur."
-NEW_BOT_UP_DATE_FOUND = (
-    "new update found for {branch_name}\n"
-    "changelog: \n\n{changelog}\n"
-    "updating your Mighty X Spam ..."
-)
-NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your Mighty X Spam...`"
-REPO_REMOTE_NAME = "temponame"
-IFFUCI_ACTIVE_BRANCH_NAME = "master"
-DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
-NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
-HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
-RESTARTING_APP = "restarting... heroku application"
-# -- Constants End -- #
 
 @Mig.on(events.NewMessage(incoming=True, pattern=r"\%supdate(?: |$)(.*)" % hl))
 @Mig2.on(events.NewMessage(incoming=True, pattern=r"\%supdate(?: |$)(.*)" % hl))
@@ -69,122 +46,169 @@ RESTARTING_APP = "restarting... heroku application"
 @Mig38.on(events.NewMessage(incoming=True, pattern=r"\%supdate(?: |$)(.*)" % hl))
 @Mig39.on(events.NewMessage(incoming=True, pattern=r"\%supdate(?: |$)(.*)" % hl))
 @Mig40.on(events.NewMessage(incoming=True, pattern=r"\%supdate(?: |$)(.*)" % hl))
-async def restart(e):
+async def update(e):
     if e.sender_id in DEV:
-        text = "Updating Your MightyXSpamBots\nExecute Ping Command After Few Seconds To check I'm Alive !!"
-        await e.reply(text, parse_mode=None, link_preview=None)
-
-
-
-@Mig.on(
-    events.NewMessage(pattern="^.update", func=lambda e: e.sender_id in DEV)
-)
-async def updater(message):
-    try:
-        repo = git.Repo()
-    except git.exc.InvalidGitRepositoryError as e:
-        repo = git.Repo.init()
-        origin = repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(IFFUCI_ACTIVE_BRANCH_NAME, origin.refs.master)
-        repo.heads.master.checkout(True)
-
-    active_branch_name = repo.active_branch.name
-    if active_branch_name != IFFUCI_ACTIVE_BRANCH_NAME:
-        await message.edit(
-            IS_SELECTED_DIFFERENT_BRANCH.format(branch_name=active_branch_name)
-        )
-        return False
-
-    try:
-        repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
-    except Exception as e:
-        print(e)
-
-    temp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
-    temp_upstream_remote.fetch(active_branch_name)
-
-    changelog = generate_change_log(
-        repo,
-        DIFF_MARKER.format(
-            remote_name=REPO_REMOTE_NAME, branch_name=active_branch_name
-        ),
-    )
-
-    if not changelog:
-        await message.edit("`Updation in Progress......`")
-        await asyncio.sleep(5)
-
-    message_one = NEW_BOT_UP_DATE_FOUND.format(
-        branch_name=active_branch_name, changelog=changelog
-    )
-    message_two = NEW_UP_DATE_FOUND.format(branch_name=active_branch_name)
-
-    if len(message_one) > 4095:
-        with open("change.log", "w+", encoding="utf8") as out_file:
-            out_file.write(str(message_one))
-        await Mig.send_message(
-            message.chat_id, document="change.log", caption=message_two
-        )
-        os.remove("change.log")
-    else:
-        await message.edit(message_one)
-
-    temp_upstream_remote.fetch(active_branch_name)
-    repo.git.reset("--hard", "FETCH_HEAD")
-
-    if HEROKU_API_KEY is not None:
-        import heroku3
-
-        heroku = heroku3.from_key(HEROKU_API_KEY)
-        heroku_applications = heroku.apps()
-        if len(heroku_applications) >= 1:
-            if HEROKU_APP_NAME is not None:
-                heroku_app = None
-                for i in heroku_applications:
-                    if i.name == HEROKU_APP_NAME:
-                        heroku_app = i
-                if heroku_app is None:
-                    await message.edit(
-                        "Invalid APP Name. Please set the name of your bot in heroku in the var `HEROKU_APP_NAME.`"
-                    )
-                    return
-                heroku_git_url = heroku_app.git_url.replace(
-                    "https://", "https://api:" + HEROKU_API_KEY + "@"
-                )
-                if "heroku" in repo.remotes:
-                    remote = repo.remote("heroku")
-                    remote.set_url(heroku_git_url)
-                else:
-                    remote = repo.create_remote("heroku", heroku_git_url)
-                asyncio.get_event_loop().create_task(
-                    deploy_start(Mig, message, HEROKU_GIT_REF_SPEC, remote)
-                )
-
-            else:
-                await message.edit(
-                    "Please create the var `HEROKU_APP_NAME` as the key and the name of your bot in heroku."
-                )
-                return
-        else:
-            await message.edit(NO_HEROKU_APP_CFGD)
-    else:
-        await message.edit("No heroku api key found in `HEROKU_API_KEY` var")
-
-
-def generate_change_log(git_repo, diff_marker):
-    out_put_str = ""
-    d_form = "%d/%m/%y"
-    for repo_change in git_repo.iter_commits(diff_marker):
-        out_put_str += f"[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
-    return out_put_str
-
-
-async def deploy_start(Mig, message, refspec, remote):
-    await message.edit(RESTARTING_APP)
-    await message.edit(
-        "__Updated your Mighty X Spam Successfully !!__"
-    )
-    await remote.push(refspec=refspec)
-    await Mig.disconnect()
-    os.execl(sys.executable, sys.executable, *sys.argv)
+        text = "**Updating Your MightyXSpamBots**\n`Please Wait For Few Seconds...`"
+        await e.reply(text, link_preview=None)
+        try:
+            await Mig.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig2.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig3.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig4.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig5.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig6.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig7.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig8.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig9.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig10.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig11.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig12.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig13.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig14.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig15.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig16.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig17.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig18.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig19.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig20.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig21.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig22.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig23.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig24.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig25.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig26.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig27.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig28.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig29.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig30.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig31.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig32.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig33.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig34.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig35.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig36.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig37.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig38.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig39.disconnect()
+        except Exception:
+            pass
+        try:
+            await Mig40.disconnect()
+        except Exception:
+            pass
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
